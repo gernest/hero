067667021e5f53589c2aabd092d0ca9f76e4cd71
gernest <geofreyernest@live.com>
2015-12-26 22:12:15 +0300

    Add JWTTokenGen
    Thes add a token generator using jwt-go which generates JWT tokens.

--- token.go
+++ token.go
@@ -1,6 +1,8 @@
 package hero
 
 import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jinzhu/now"
 	"github.com/pborman/uuid"
 )
 
@@ -10,4 +12,20 @@ type SimpleTokenGen struct{}
 // Generate returns a UUID v4 string
 func (s *SimpleTokenGen) Generate() string {
 	return uuid.NewRandom().String()
+}
+
+type JWTTokenGen struct {
+	publicKey  []byte
+	privateKey []byte
+}
+
+func (j *JWTTokenGen) Generate() string {
+	exp := now.EndOfYear()
+	token := jwt.New(jwt.SigningMethodRS256)
+	token.Claims["exp"] = exp.Unix()
+	tok, err := token.SignedString(j.privateKey)
+	if err != nil {
+		panic(err)
+	}
+	return tok
 }

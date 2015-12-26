44fcc2c2508343bcdff0946f1ac078a5e0f5bc80
gernest <geofreyernest@live.com>
2015-12-26 22:22:21 +0300

    Fix golint errors

--- token.go
+++ token.go
@@ -14,15 +14,20 @@ func (s *SimpleTokenGen) Generate() string {
 	return uuid.NewRandom().String()
 }
 
+// JWTTokenGen implements TokenGenerator interface for JWT tokens.
 type JWTTokenGen struct {
 	publicKey  []byte
 	privateKey []byte
 }
 
+//NewJWTGen returns a new JWT token generater which signs the toke
+// with private key. This uses RSA keys.
 func NewJWTGen(public, private []byte) *JWTTokenGen {
 	return &JWTTokenGen{publicKey: public, privateKey: private}
 }
 
+// Generate generates new jwt tokens, only claim is expire date which is after one
+// year.
 func (j *JWTTokenGen) Generate() string {
 	exp := now.EndOfYear()
 	token := jwt.New(jwt.SigningMethodRS256)
